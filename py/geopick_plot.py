3ead3103e43f98dbb7b8c3f7e2f3a3aaff3b1246
Arnald Marcer <arnald.marcer@uab.cat>
2024-02-21 15:11:51 +0100

    notebook plots

--- py/geopick_plot.py
+++ py/geopick_plot.py
@@ -4,6 +4,7 @@ import shapely
 import matplotlib.pyplot as plt
 import contextily as ctx
 import matplotlib.ticker as ticker
+from matplotlib.markers import MarkerStyle
 import zipfile
 import os
 
@@ -34,6 +35,9 @@ def get_plot_params():
                  "unc_line_color": "blue",
                  "vertex_color": "black",
                  "candidate_color": "yellow",
+                 "candidate_sa_color": "magenta",
+                 'candidate_sa_marker': ".",
+                 'candidate_sa_size': 10,
                  "nearest_point_color": "red",
                  "nearest_point_marker": "+",
                  "nearest_point_size": 75,
@@ -62,7 +66,7 @@ def plot_geopick_location(location, params):
   ctx.add_basemap(ax, crs=location.crs.to_string(), source=ctx.providers.OpenStreetMap.Mapnik)
   location.plot(aspect = p['aspect'], facecolor = "none", edgecolor = "blue", ax = ax)
 
-def plot_geopick(sec, location, centroid, uncertainty_line, uncertainty_value, vertices, candidates, nearest_point, nearest_segment, basemap, xlim, ylim, params):
+def plot_geopick(sec, location, centroid, uncertainty_line, uncertainty_value, vertices, candidates, candidates_sa, nearest_point, nearest_segment, basemap, xlim, ylim, params):
   if params is not None:
     p = params
   else:
@@ -91,6 +95,8 @@ def plot_geopick(sec, location, centroid, uncertainty_line, uncertainty_value, v
     vertices.plot(aspect = p['aspect'], color = p['vertex_color'], markersize = 10, ax = ax)
   if candidates is not None:
     candidates.plot(aspect = p['aspect'], color = p['candidate_color'], markersize = 20, ax = ax)    
+  if candidates_sa is not None:
+    candidates_sa.plot(aspect = p['aspect'], edgecolor = p['candidate_sa_color'], color = 'none', markersize = p['candidate_sa_size'], marker = p['candidate_sa_marker'], ax = ax)    
   if nearest_point is not None:
     nearest_point.plot(aspect = p['aspect'], color = p['nearest_point_color'], 
                        marker = p['nearest_point_marker'],
